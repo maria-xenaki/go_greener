383cc076562bdbfccb00f2884e1045489c3e3fa5
maria-xenaki <mariaaxenaki@gmail.com>
2025-09-18 14:30:03 +0300

    route and message fixes

--- frontend/src/pages/AuthPage.js
+++ frontend/src/pages/AuthPage.js
@@ -1,11 +1,12 @@
 import LoginForm from "../components/LoginForm";
+import { Link } from "react-router-dom";
 
 export default function AuthPage() {
 
     return (
         <div>
             <LoginForm/>
-            <p className="text-center">Not a member yet? Sign up <a href="/register">here</a>.</p>
+            <p className="text-center">Not a member yet? Sign up <Link to="/register">here</Link>.</p>
         </div>
     );
 }
