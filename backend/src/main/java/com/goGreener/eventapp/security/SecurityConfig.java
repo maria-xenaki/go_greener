f4c900bd45290e2840cc30cd525a9d2ac896a247
maria-xenaki <mariaaxenaki@gmail.com>
2025-09-22 10:07:42 +0300

    url fix

--- backend/src/main/java/com/goGreener/eventapp/security/SecurityConfig.java
+++ backend/src/main/java/com/goGreener/eventapp/security/SecurityConfig.java
@@ -94,7 +94,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://go-greener-app.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
